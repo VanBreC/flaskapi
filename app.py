059121511971173ba81de7fa802fe8dd315791ca
vanbrec <vanbrec@dunwoody.edu>
2020-12-09 22:42:06 +0000

    brendans first commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 #mongo
-app.config["MONGO_URI"] = "mongodb://192.168.80.100:27017/temperature"
+app.config["MONGO_URI"] = "mongodb://192.168.56.103:27017/temperature"
 mongo = PyMongo(app)
 
 @app.route('/')
